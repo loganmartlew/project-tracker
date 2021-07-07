54ebfa80e6ca99d4bdad6ca04390d43bafedd3d3
Logan Martlew <logan.martlew@gmail.com>
2021-07-08 03:39:49 +1200

    Moved environment variables to .env file

--- next.config.js
+++ next.config.js
@@ -1,10 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  env: {
-    MONGO_URI:
-      'mongodb+srv://root:Jagjagger1@projectsdb.ocbhy.mongodb.net/ProjectsDB?retryWrites=true&w=majority',
-    ADMIN_EMAIL: 'logan.martlew@gmail.com',
-    ADMIN_PASSWORD:
-      '$2a$12$bJF1jtvQs227BBOkzTQpSuQ8Y77IqPvpdzsp1j1L9/8oUK5RNXU7W',
-  },
 };
